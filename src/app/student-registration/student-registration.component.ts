71e05e13511baa4e4a29811a5e731fe8a1d6cdf7
ou786 <osamamohammed.ussaid@dentistryautomation.com>
2024-06-26 22:57:23 +0530

    a trial

---
+++ src/app/student-registration/student-registration.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-student-registration',
+  templateUrl: './student-registration.component.html',
+  styleUrls: ['./student-registration.component.css']
+})
+export class StudentRegistrationComponent {
+  student = {
+    name: '',
+    dob: '',
+    gender: '',
+    address: '',
+    ph: '',
+    grade: '',
+    parentsName: ''
+    // Add more properties for other student information
+  };
+
+  students: any[] = [];
+
+  onSubmit() {
+    this.students.push({ ...this.student });
+    this.student = {
+      name: '',
+      dob: '',
+      gender: '',
+      address: '',
+    ph: '',
+    grade: '',
+    parentsName: ''
+    };
+  }
+}
